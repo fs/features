45d8f02941b2aac90e7f43a231b6afb2996b073b
Timur Vafin <timur.vafin@flatsoft.com>
2010-07-27 18:33:55 +0400

    Rename app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails3Base::Application.configure do
+Features::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The production environment is meant for finished, "live" apps.
